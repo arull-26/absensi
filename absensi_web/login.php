19f80e389087ed16fb5dd2ec1655a05e2a09cae0
arull-26 <rullge64@gmail.com>
2025-12-05 19:05:23 +0700

    nambah emot doang

--- absensi_web/login.php
+++ absensi_web/login.php
@@ -37,7 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Absensi Karyawan</title>
+    <title>Login Absensi Karyawan 🌐.</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <style>
         /* CSS Tambahan untuk Login */
